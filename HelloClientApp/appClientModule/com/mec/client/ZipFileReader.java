0633c50b9bedf8aa72e2b650600f16ce3e3a12bc
mec_test_1 <mec_test_1@hotmail.com>
2015-11-27 13:35:02 +0800

    Convert ZipFileReader into utility class;

--- HelloClientApp/appClientModule/com/mec/client/ZipFileReader.java
+++ HelloClientApp/appClientModule/com/mec/client/ZipFileReader.java
@@ -3,56 +3,75 @@ package com.mec.client;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.PrintStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-import javax.ws.rs.Path;
-
 public class ZipFileReader {
 
+	/**
+	 * <p>
+	 * List file items in ZIP file recursively;
+	 * </p>
+	 * @param zipFilePath zip file full path
+	 * @param tempDir temporary directory, default to the same directory as zipFilePath;
+	 * @throws Exception
+	 */
 	public static void unzipList(String zipFilePath, String tempDir) throws Exception{
+		path = zipFilePath;
+		pathDir = tempDir;
 		
+		File zipFile = new File(path);
+		
+		if(!zipFile.exists()){
+			throw new FileNotFoundException(String.format("File %s not found.", zipFilePath));
+		}
+		if(null == tempDir){
+			tempDir = zipFile.getParent();
+		}
 		File pathDirectory = new File(pathDir);
 		if(!pathDirectory.exists()){
 			pathDirectory.mkdirs();
 		}
 		
-		extractZipFile(new File(path));
+		extractZipFile(zipFile);
 	}
 	
-	public static void extractZipFile(File path) throws Exception{
+	private static void extractZipFile(File path) throws Exception{
 		int bufferSize = 2048;
 		int count = 0;
 //		out.printf("----------------zip file: %s\n", path);
 		FileInputStream fis = new FileInputStream(path);
-		ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
-		ZipEntry entry;
-		FileOutputStream fos = null;
-		File tmpFile = null;
-		byte[] data = null;
-		while(null != (entry = zis.getNextEntry())){
-//			out.println(entry.getName());
-			if(!isZipFile(entry.getName())){
-				continue;
-			}
-//				bufferSize = entry.getSize();
-			count = 0;
-			data = new byte[bufferSize];
-//			tmpFile = File.createTempFile(entry.getName(), "tmp", pathDirectory);
-			tmpFile = new File(String.format("%s%s%s", pathDir, entry.getName().replaceAll("/", "_"), System.currentTimeMillis()));
-			fos = new FileOutputStream(tmpFile);
-//				ByteOutputStream ois = new ByteArrayOutputStream();
-			while(-1 != (count = zis.read(data, 0, bufferSize))){
-				fos.write(data, 0, count);
-			}
-			if(null != fos){
-				fos.close();
-			}
-			
-			zis.closeEntry();
-			extractZipFile(tmpFile);
+		
+		try(ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));){
+			ZipEntry entry;
+			FileOutputStream fos = null;
+			File tmpFile = null;
+			byte[] data = null;
+			while (null != (entry = zis.getNextEntry())) {
+				//			out.println(entry.getName());
+				if (!isZipFile(entry.getName())) {
+					continue;
+				}
+				//				bufferSize = entry.getSize();
+				count = 0;
+				data = new byte[bufferSize];
+				//			tmpFile = File.createTempFile(entry.getName(), "tmp", pathDirectory);
+				tmpFile = new File(String.format("%s%s%s", pathDir, entry.getName().replaceAll("/", "_"),
+						System.currentTimeMillis()));
+				fos = new FileOutputStream(tmpFile);
+				//				ByteOutputStream ois = new ByteArrayOutputStream();
+				while (-1 != (count = zis.read(data, 0, bufferSize))) {
+					fos.write(data, 0, count);
+				}
+				if (null != fos) {
+					fos.close();
+				}
+
+				zis.closeEntry();
+				extractZipFile(tmpFile);
+			} 
 		}
 	}
 	
